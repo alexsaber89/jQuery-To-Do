747bb1847c7a0855deaf66af407be76052d8a4c6
Alex Saber <Alex Saber>
2016-10-29 12:09:34 -0500

    added event listeners for edit and delete buttons

--- javascripts/ToDo.js
+++ javascripts/ToDo.js
@@ -1,16 +1,28 @@
 "use strict";
 
 let ToDo = (function() {
+  let body = $("#body");
   let toDoDiv = $("#to-do");
+  let toDoDivArray = [];
   let completedDiv = $("#completed");
+  let completedDivArray = [];
   let addBtn = $("#add-btn");
   let userInput = "";
-  let toDoItemsArray = [];
   let uniqueID = 0;
 
   return {
     addEvents: function() {
       addBtn.on("click",ToDo.getUserInput);
+      body.on("click", function(event) {
+        if ($(event.target).hasClass("delete-btn")) {
+          console.log("delete button clicked");
+        }
+      });
+      body.on("click", function(event) {
+        if ($(event.target).hasClass("edit-btn")) {
+          console.log("edit button clicked");
+        }
+      });
     },
     getUserInput: function(event) {
       userInput = $("#userInput").val();
@@ -25,14 +37,22 @@ let ToDo = (function() {
         "id": uniqueID,
         "text": userInput
       };
-      toDoItemsArray.push(userToDoObject);
-      console.log("toDoItemsArray: ",toDoItemsArray);
-      ToDo.displayUserMessages();
+      toDoDivArray.push(userToDoObject);
+      console.log("toDoDivArray: ",toDoDivArray);
+      ToDo.displayUserToDoItems();
     },
-    displayUserMessages: function() {
-      toDoItemsArray.forEach((object) => {
+    displayUserToDoItems: function() {
+      let displayedToDoItems = "";
+      displayedToDoItems += `<h2>To Do</h2>`;
+      toDoDivArray.forEach((object) => {
         console.log(object.text);
+        displayedToDoItems += `<button type="button" class="btn btn-primary btn-sm edit-btn">Edit</button> `;
+        displayedToDoItems += `<button type="button" class="btn btn-primary btn-sm delete-btn">Delete</button>`;
+        displayedToDoItems += `${object.text}`;
+        displayedToDoItems += `<br /><br />`;
       });
+      toDoDiv.html(displayedToDoItems);
+      completedDiv.html(displayedToDoItems);
     }
   };
 })();
