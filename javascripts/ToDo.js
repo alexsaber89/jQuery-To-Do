be0f056d6d569b81ef95fd27ab4ca355bbaa8e38
Alex Saber <Alex Saber>
2016-10-31 00:02:31 -0500

    user now able to complete their to-do items

--- javascripts/ToDo.js
+++ javascripts/ToDo.js
@@ -29,35 +29,55 @@ let ToDo = (function() {
           console.log("edit button has been clicked");
         }
       });
+      body.on("change", function(event) {
+        if ($(event.target).hasClass("checkbox")) {
+          let targetedToDoId = $(event.target).closest(".to-do-wrapper").attr("id");
+          for (var i = 0; i < toDoDivArray.length; i++) {
+            if (parseInt(toDoDivArray[i].id) === parseInt(targetedToDoId)) {
+                let targetedToDoObject = toDoDivArray[i];
+                completedDivArray.push(targetedToDoObject);
+                console.log("completedDivArray: ",completedDivArray);
+                ToDo.displayUserToDoItems(completedDivArray,completedDiv);
+                toDoDivArray.splice(toDoDivArray.indexOf(targetedToDoObject),1);
+                $(event.target).closest(".to-do-wrapper").remove();
+            }
+          }
+        }
+      });
     },
     getUserInput: function(event) {
       if (userInput.val() !== "") {
         uniqueID++;
-        ToDo.setUserToDoObject(uniqueID,userInput.val());
+        let whichArray = "toDoDivArray";
+        ToDo.setUserToDoObject(uniqueID,userInput.val(),whichArray);
         userInput.val("");
         userInput.focus();
       }
     },
-    setUserToDoObject: function(uniqueID,userInput) {
+    setUserToDoObject: function(uniqueID,userInput,whichArray) {
       var userToDoObject = {
         "id": uniqueID,
         "text": userInput
       };
-      toDoDivArray.push(userToDoObject);
-      ToDo.displayUserToDoItems();
+      if (whichArray === "toDoDivArray") {
+        toDoDivArray.push(userToDoObject);
+      } else if(whichArray === "completedDivArray") {
+        completedDivArray.push(userToDoObject);
+      }
+      ToDo.displayUserToDoItems(toDoDivArray,toDoDiv);
     },
-    displayUserToDoItems: function() {
+    displayUserToDoItems: function(array,displayDiv) {
       let displayedToDoItems = "";
-      toDoDivArray.forEach((object) => {
-        displayedToDoItems += `<div> `;
-        displayedToDoItems += `<div class="checkbox"><label><input type="checkbox" value=""></label></div>`;
+      array.forEach((object) => {
+        displayedToDoItems += `<div id=${object.id} class="to-do-wrapper"> `;
+        displayedToDoItems += `<div class="checkbox-style"><label><input class="checkbox" type="checkbox" value=""></label></div>`;
         displayedToDoItems += `<button type="button" class="btn btn-primary btn-sm edit-btn">Edit</button> `;
         displayedToDoItems += `<button type="button" class="btn btn-primary btn-sm delete-btn">Delete</button> `;
-        displayedToDoItems += `${object.text}`;
+        displayedToDoItems += `<div class="to-do_text">${object.text}</div>`;
         displayedToDoItems += `<br />`;
         displayedToDoItems += `</div>`;
       });
-      toDoDiv.html(displayedToDoItems);
+      displayDiv.html(displayedToDoItems);
     }
   };
 })();
